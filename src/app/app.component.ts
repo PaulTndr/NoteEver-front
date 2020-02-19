3ec4f62ba23b076e61972cf5f25efee46196a1ab
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2020-02-19 12:17:02 +0100

    Chargement lancement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NoteManagmentService } from './NoteManagment.service';
+import { Note } from 'src/models/Note';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'NoteEver';
+  isLoaded=false;
+  isAnimationClosed=false;
+  listNotesSubscription: Subscription;
+  
+  constructor(private router : Router, private noteManagmentService : NoteManagmentService){}
+  ngOnInit(){
+    this.noteManagmentService.getNotes();
+    this.listNotesSubscription = this.noteManagmentService.listNotesSubject.subscribe(
+      (listNotes: Note[]) => {
+        if(listNotes.length!=0){
+          this.isLoaded=true;
+          setTimeout(()=>{
+            this.isAnimationClosed=true;
+          },400);
+        }
+      }
+    );
+  }
 }
